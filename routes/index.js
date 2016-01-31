3fc859af734e8c2d75066e1efcdadb56c6325545
joneqian <qian.qing@aliyun.com>
2016-01-31 21:24:37 +0800

    apply page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '首页 - 大内购 - 联盟企业员工的内部福利' });
 });
 
+router.get('/apply', function(req, res, next) {
+  res.render('apply', { title: '企业申请 - 大内购 - 联盟企业员工的内部福利' });
+});
+
 module.exports = router;
