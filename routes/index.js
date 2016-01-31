7ea095e3e782e2c5e5cc325c66e8a4c9970589f4
joneqian <qian.qing@aliyun.com>
2016-01-31 15:30:47 +0800

    add project file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '首页 - 大内购 - 联盟企业员工的内部福利' });
 });
 
 module.exports = router;
